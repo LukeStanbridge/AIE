8fb92475f2c1256d1d4f323d88cd4bad225fb8f7
LukeStanbridge <99298136+LukeStanbridge@users.noreply.github.com>
2022-05-06 11:47:36 +0930

    completed references exercises

--- Diploma/Introduction to C++/References/References.cpp
+++ Diploma/Introduction to C++/References/References.cpp
@@ -2,11 +2,63 @@
 //
 
 #include <iostream>
+//int main()
+//{
+//    // EXERCISE 1
+//    /*int variable = 16;
+//    float variable1 = 17;
+//    std::cout << variable << std::endl;
+//    int& reference = variable;
+//    std::cout << reference << std::endl;
+//    int& reference1 = variable;
+//    std::cout << reference1 << std::endl;
+//    int& reference2 = reference;
+//    std::cout << reference2 << std::endl;*/
+//    // int to float doesnt work
+//    /*int& reference3 = variable1;
+//    std::cout << reference3 << std::endl;*/
+//
+//    // EXERCISE 2
+//    /*int var1 = 10;
+//    int var2 = 20;
+//    std::cout << var1 << std::endl << var2 << std::endl;
+//    Function(var1, var2);
+//    std::cout << var1 << std::endl << var2 << std::endl;*/
+
+// EXERCISE 3
+struct Person
+{
+    int age;
+    float height;
+    char initials;
+};
+
+void Function(/*const*/Person& p)
+{
+    p.age = 34;
+    p.height = 210.34f;
+    p.initials = 'K';
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Person p1;
+    p1.age = 32;
+    p1.height = 195.4f;
+    p1.initials = 'L';
+
+    // before
+    std::cout << p1.age << std::endl << p1.height << std::endl << p1.initials << std::endl;
+    std::cout << std::endl;
+
+    Function(p1);
+
+    //after
+    std::cout << p1.age << std::endl << p1.height << std::endl << p1.initials << std::endl;
+    std::cout << std::endl;
 }
+
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
